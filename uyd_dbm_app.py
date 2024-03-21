3d24388cd5e69c01a6418cc4e6d4f83beed30b7f
Tito Osadebey <94079649+titoausten@users.noreply.github.com>
2024-03-21 10:06:45 +0100

    Update uyd_dbm_app.py

--- uyd_dbm_app.py
+++ uyd_dbm_app.py
@@ -3,6 +3,7 @@ from utils import *
 # set_background('./nba_rookie1.jpg')
 
 config['credentials']['usernames']['uyd2023']['password'] = hashed_password[0]
+config['credentials']['usernames']['uyd2023admin']['password'] = hashed_password[1]
 
 
 def main():
@@ -17,10 +18,16 @@ def main():
                                                                         'Login': 'Login'})
 
     if authentication_status:
-        authenticator.logout('Logout', 'sidebar')
-        st.sidebar.title('Welcome!')
-        st.title(':green[UMUEHEA YOUTHS IN DIASPORA]')
-        call_to_action()
+        if username == 'uyd2023admin':
+            authenticator.logout('Logout', 'sidebar')
+            st.sidebar.title('Welcome Admin!')
+            st.title(':green[UMUEHEA YOUTHS IN DIASPORA]')
+            call_to_action("admin")
+        elif username == 'uyd2023admin':
+            authenticator.logout('Logout', 'sidebar')
+            st.sidebar.title('Welcome!')
+            st.title(':green[UMUEHEA YOUTHS IN DIASPORA]')
+            call_to_action("member")
     elif authentication_status is False:
         st.error('Username or Password is incorrect')
     elif authentication_status is None:
