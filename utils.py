fe0d51cb9af25e9af65117d27aa0fa77d03ebf54
Tito Osadebey <94079649+titoausten@users.noreply.github.com>
2024-05-03 09:21:54 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import pickle
 from pathlib import Path
 import yaml
 from yaml.loader import SafeLoader
-import streamlit_authenticator as stauth
+# import streamlit_authenticator as stauth
 
 
 # CONSTANTS
